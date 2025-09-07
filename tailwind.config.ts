b06101967e3df6ef45695a4b049a74e80d7aca6c
1Hacker-anonimo <160126765+1Hacker-anonimo@users.noreply.github.com>
2025-09-07 19:11:59 -0300

    Atualizar o tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -92,7 +92,7 @@ export default {
 				},
 				float: {
 					'0%, 100%': { transform: 'translateY(0px)' },
-					'50%': { transform: 'translateY(-20px)' }
+					'100%': { transform: 'translateY(-0px)' }
 				},
 				pulse: {
 					'0%, 100%': { opacity: '1' },
