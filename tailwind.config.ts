1d8aabce7cec1e819c027fd195b25b838cb8b004
1Hacker-anonimo <160126765+1Hacker-anonimo@users.noreply.github.com>
2025-09-07 19:17:47 -0300

    Atualizar o tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -103,7 +103,7 @@ export default {
 				'accordion-down': 'accordion-down 0.2s ease-out',
 				'accordion-up': 'accordion-up 0.2s ease-out',
 				'float': 'float 3s ease-in-out infinite',
-				'pulse-slow': 'pulse 3s ease-in-out infinite'
+				'pulse-slow': 'pulse 0s ease-in-out infinite'
 			}
 		}
 	},
