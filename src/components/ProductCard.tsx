e4fb29e6e290e9115c274460bf9cec96d7fc3602
1Hacker-anonimo <160126765+1Hacker-anonimo@users.noreply.github.com>
2025-09-07 21:33:11 -0300

    Update ProductCard.tsx

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -58,7 +58,7 @@ const ProductCard = ({ product, onProductClick }: ProductCardProps) => {
         </div>
         
         <div className="flex gap-2">
-          <Button size="sm" className="flex-1 bg-white text-white hover:bg-white font-medium">
+          <Button size="sm" className="flex-1 bg-white text-black hover:bg-gray-200 font-medium">
             <ShoppingBag className="h-4 w-4 mr-2" />
             Adicionar ao Carrinho
           </Button>
